a6945c5c2ade0fd7f8dff7b4f34a2718b2952f76
Meeso1 <39009805+Meeso1@users.noreply.github.com>
2026-06-30 21:45:54 +0200

    More improvements

--- src/components/about-me-card.tsx
+++ src/components/about-me-card.tsx
@@ -17,9 +17,22 @@ export default function AboutMeCard({ className }: { className?: string }) {
         <CardTitle>About Me</CardTitle>
       </CardHeader>
       <CardContent>
-        I'm a backend dev who currently only codes at work. 
+        I'm a backend dev who currently only codes at work.
         To change that, I'll be creating random small projects and publishing them here, while also learning some fullstack.
+        <br/>
+        <br/>
+        I plan to create:
+        <ul>
+          <li>- Gimmck tools that are probably not too useful</li>
+          <li>- Small games / -dle's</li>
+          <li>- Some ML projects / experiments</li>
+          <li>- Some other stuff probably...</li>
+        </ul>
+        <br/>
         New stuff will show up here (ir)regularly.
+        <br/>
+        <br/>
+        If you thing the website is hideous, that will also change - I plan to redesign it every so often to perhaps learn some frontend fundamentals.
       </CardContent>
       <CardFooter className="flex-col gap-2">
         <LinkButton href="https://github.com/Meeso1">Meeso1 @ GitHub</LinkButton>
--- src/components/project-carousel.tsx
+++ src/components/project-carousel.tsx
@@ -8,25 +8,36 @@ import { cn } from "@/lib/utils";
 export default function ProjectCarousel(
     { projects, className }: { projects: { title: string, description: string, link: string }[], className?: string }
 ) {
+    const DURATION = 250;
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [animationKey, setAnimationKey] = useState(0);
+    const [busy, setBusy] = useState(false);
+
+    function rotate(change: number) {
+        if (busy) return;
+        setBusy(true);
+        
+        setAnimationKey((key) => key + 1);
+        setTimeout(() => {
+            setCurrentIndex((currentIndex + change + projects.length) % projects.length);
+        }, DURATION / 2);
+        setTimeout(() => setBusy(false), DURATION);
+    }
 
     return (
-        <div className={cn("flex items-center justify-center", className)}>
+        <div className={cn("flex items-center justify-center perspective-distant", className)}>
             <div className="w-1/6 h-full flex items-center justify-center">
                 <Button className="w-1/12 h-16 bg-gray-800 opacity-50" 
-                    onClick={() => setCurrentIndex((currentIndex - 1 + projects.length) % projects.length)}>
+                    onClick={() => rotate(-1)}>
                     <b>&lt;</b>
                 </Button>
             </div>
-            <ProjectCard 
-                title={projects[currentIndex].title} 
-                description={projects[currentIndex].description} 
-                link={projects[currentIndex].link} 
-                className="h-full w-full"
-            />
+            <div key={animationKey} className="h-full w-full animate-flip-next">
+                <ProjectCard {...projects[currentIndex]} className="h-full w-full" />
+            </div>
             <div className="w-1/6 h-full flex items-center justify-center">
                 <Button className="w-1/12 h-16 bg-gray-800 opacity-50" 
-                    onClick={() => setCurrentIndex((currentIndex + 1) % projects.length)}>
+                    onClick={() => rotate(1)}>
                     <b>&gt;</b>
                 </Button>
             </div>
